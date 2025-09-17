cb8ca872d2046ece012c4969cfde89e2be3aca83
david-solomon-henshaw <david.solomon.henshaw@gmail.com>
2025-09-17 21:47:21 +0100

    feat: Implement database connection with Mongoose This commit introduces the logic to connect the Express server to a MongoDB database. It uses Mongoose for simplified interaction and dotenv for secure handling of the connection URI

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,37 @@
 // Import the Express module to create a web server
 const express = require('express');
 
+
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
 // Initialize the Express application
 const app = express();
+
+const connectDb = async () => {
+
+    try {
+
+        await mongoose.connect(process.env.Mongo_Uri)
+        console.log('Connected to database')
+        
+        // Start the server and make it listen on port 4000
+        app.listen(4000, () => {
+            // Log a message to the console once the server is running
+            console.log('Server is running on port 4000');
+        });
+
+
+    } catch  (error) {
+        console.log(error)
+
+    }
+}
+
+
+connectDb();
+
 
 // Define a route for HTTP GET requests to the root path ('/')
 app.get('/', (req, res) => {
@@ -10,8 +39,4 @@ app.get('/', (req, res) => {
    res.send('Server is Running');
 });
 
-// Start the server and make it listen on port 4000
-app.listen(4000, () => {
-    // Log a message to the console once the server is running
-    console.log('Server is running');
-});
+
